c655b4cbb7a1ff8578c7dce56cf9be276a5d63d4
marwan-elabhar <marwan.e.elabhar@gmail.com>
2025-12-25 12:49:55 +0200

    implement ModalView component

---
+++ src/components/ModalView.vue
@@ -0,0 +1,28 @@
+<script>
+import useKeydown from "@/composables/use-keydown";
+export default {
+  name: "ModalView",
+  setup(props, {emit}) {
+    useKeydown([{key: 'Escape', fn: () => {emit('closeModal')}}])
+
+    return {
+      emit
+    }
+
+  }
+}
+</script>
+
+<template>
+  <div class="modal">
+    <div class="overlay" @click="$emit('closeModal')">
+      <div class="modal-card">
+        <slot />
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+
