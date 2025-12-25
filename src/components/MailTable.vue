f6a4aca3dfc213a44f9dc6c9fd562d4d8412228c
marwan-elabhar <marwan.e.elabhar@gmail.com>
2025-12-25 14:01:45 +0200

    extract logic into useEmailSelection composable

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -3,7 +3,9 @@ import {format} from 'date-fns'
 import axios from 'axios'
 import MailView from './MailView.vue'
 import ModalView from "./ModalView.vue";
-import {ref, reactive} from "vue";
+import {ref} from "vue";
+import useEmailSelection from "../composables/use-email-selection"
+
 
 export default {
 
@@ -12,21 +14,10 @@ export default {
   async setup() {
     let {data: emails} = await axios.get('http://localhost:3000/emails')
 
-    let selected = reactive(new Set())
-    let emailSelection = {
-      emails: selected,
-      toggle(email) {
-        if (selected.has(email)) {
-          selected.delete(email)
-        } else {
-          selected.add(email)
-        }
-      }
-    }
 
 
     return {
-      emailSelection,
+      emailSelection: useEmailSelection(),
       format,
       emails: ref(emails),
       openedEmail: ref(null)
