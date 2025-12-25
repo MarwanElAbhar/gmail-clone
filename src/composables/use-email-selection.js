2bd6979137fec80a678a98ca69756742096fcaf4
marwan-elabhar <marwan.e.elabhar@gmail.com>
2025-12-25 14:45:29 +0200

    add new functionalities in useEmailSelection composable

--- src/composables/use-email-selection.js
+++ src/composables/use-email-selection.js
@@ -1,4 +1,5 @@
 import {reactive} from "vue"
+import axios from "axios";
 let emails = reactive(new Set())
 
 export const useEmailSelection = () => {
@@ -21,11 +22,27 @@ export const useEmailSelection = () => {
         })
     }
 
+    let forSelected = (fn) => {
+        emails.forEach(email => {
+            fn(email)
+            axios.put(`http://localhost:3000/emails/${email.id}`, email)
+        })
+    }
+
+    let markRead = () => forSelected(e => e.read = true)
+
+    let markUnread = () => forSelected(e => e.read = false)
+
+    let archiveEmail = () => forSelected(e => e.archived = true)
+
     return {
         emails,
         toggle,
         clear,
         addMultiple,
+        markRead,
+        markUnread,
+        archiveEmail
     }
 }
 
