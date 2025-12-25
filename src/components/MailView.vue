199631f87404f2d2c8e287b7cf24a23be20f7884
marwan-elabhar <marwan.e.elabhar@gmail.com>
2025-12-25 13:45:28 +0200

    implement buttons functionality in MailView

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -6,24 +6,53 @@ import useKeydown from "../composables/use-keydown";
 
 export default {
   name: "MailView",
-  setup(props) {
-    // dsds
+  setup(props, {emit}) {
     let email = props.email
+    // let toggleRead = () => {
+    //   email.read = !email.read
+    //   axios.put(`http://localhost:3000/emails/${email.id}`, email)
+    // }
+    //
+    // let toggleArchive = () => {
+    //   email.archived = !email.archived
+    //   axios.put(`http://localhost:3000/emails/${email.id}`, email)
+    // }
     let toggleRead = () => {
-      email.read = !email.read
-      axios.put(`http://localhost:3000/emails/${email.id}`, email)
+      emit('changeEmail', {toggleRead: true, save: true})
     }
-
     let toggleArchive = () => {
-      email.archived = !email.archived
-      axios.put(`http://localhost:3000/emails/${email.id}`, email)
+      emit('changeEmail', {toggleArchive: true, save: true, closeModal: true})
+    }
+    let goNewer = () => {
+      emit('changeEmail', {changeIndex: -1})
+    }
+    let goOlder = () => {
+      emit('changeEmail', {changeIndex: +1})
+    }
+    let goNewerAndArchive = () => {
+      emit('changeEmail', {changeIndex: -1, toggleArchive: true, save: true})
+    }
+    let goOlderAndArchive = () => {
+      emit('changeEmail', {changeIndex: 1, toggleArchive: true, save: true})
     }
 
-    useKeydown([{key: 'r', fn: toggleRead}])
+
+    useKeydown([
+      {key: 'r', fn: toggleRead},
+      {key: 'e', fn: toggleArchive},
+      {key: 'k', fn: goNewer},
+      {key: 'j', fn: goOlder},
+      {key: '[', fn: goNewerAndArchive},
+      {key: ']', fn: goOlderAndArchive},
+
+    ])
     return {
       format,
       marked,
-      toggleRead
+      toggleRead,
+      toggleArchive,
+      goOlder,
+      goNewer,
     }
   },
   props: {
@@ -32,19 +61,20 @@ export default {
       required: true
     }
   }
+
 }
 </script>
 
 <template>
   <div class="email-display">
     <div>
-<!--      <button @click="toggleArchive">{{email.archived ? 'Move to inbox (e)' : 'Archive (e)'}}</button>-->
-<!--      <button @click="toggleRead">{{email.read ? 'Mark Unread (r)' : 'Mark Read (r)'}}</button>-->
-<!--      <button @click="goNewer">Newer (k)</button>-->
-<!--      <button @click="goOlder">Older</button>-->
+      <button @click="toggleArchive">{{ email.archived ? 'Move to inbox (e)' : 'Archive (e)' }}</button>
+      <button @click="toggleRead">{{ email.read ? 'Mark Unread (r)' : 'Mark Read (r)' }}</button>
+      <button @click="goNewer">Newer (k)</button>
+      <button @click="goOlder">Older (j)</button>
     </div>
-    <h2 class="mb-0">Subject: <strong>{{email.subject}}</strong></h2>
-    <div><em>From {{email.from}} on {{format(new Date(email.sentAt), 'MMM do yyyy')}}</em></div>
+    <h2 class="mb-0">Subject: <strong>{{ email.subject }}</strong></h2>
+    <div><em>From {{ email.from }} on {{ format(new Date(email.sentAt), 'MMM do yyyy') }}</em></div>
     <div v-html="marked(email.body)"></div>
   </div>
 
