e5aca6531890df744574f8be998cc9c1e5d405ea
marwan-elabhar <marwan.e.elabhar@gmail.com>
2025-12-25 12:49:45 +0200

    implement MailView component

---
+++ src/components/MailView.vue
@@ -0,0 +1,55 @@
+<script>
+import {format} from "date-fns";
+import {marked} from 'marked'
+import axios from "axios";
+import useKeydown from "@/composables/use-keydown";
+
+export default {
+  name: "MailView",
+  setup(props) {
+    // dsds
+    let email = props.email
+    let toggleRead = () => {
+      email.read = !email.read
+      axios.put(`http://localhost:3000/emails/${email.id}`, email)
+    }
+
+    let toggleArchive = () => {
+      email.archived = !email.archived
+      axios.put(`http://localhost:3000/emails/${email.id}`, email)
+    }
+
+    useKeydown([{key: 'r', fn: toggleRead}])
+    return {
+      format,
+      marked,
+      toggleRead
+    }
+  },
+  props: {
+    email: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="email-display">
+    <div>
+<!--      <button @click="toggleArchive">{{email.archived ? 'Move to inbox (e)' : 'Archive (e)'}}</button>-->
+<!--      <button @click="toggleRead">{{email.read ? 'Mark Unread (r)' : 'Mark Read (r)'}}</button>-->
+<!--      <button @click="goNewer">Newer (k)</button>-->
+<!--      <button @click="goOlder">Older</button>-->
+    </div>
+    <h2 class="mb-0">Subject: <strong>{{email.subject}}</strong></h2>
+    <div><em>From {{email.from}} on {{format(new Date(email.sentAt), 'MMM do yyyy')}}</em></div>
+    <div v-html="marked(email.body)"></div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
